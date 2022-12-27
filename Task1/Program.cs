e931749286ea20e3687c7d3b5a685d4a244f3f1b
Konstantin <kurakin-1993@mail.ru>
2022-12-27 22:31:48 +0400

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Task1;
+{
+    {
+        var classes = new[]
+              {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+        var allStudents = GetAllStudents(classes);
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+        var groupClasess = classes.SelectMany(_ => _.Students);
+        return groupClasess.ToArray();
+    }
+}
+
